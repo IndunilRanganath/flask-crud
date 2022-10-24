3df997a1f515e6ff65c60ed78060539fcf30eeb5
IndunilRanganath <indunilranganath071@gmail.com>
2022-10-24 20:20:21 +0530

    GET method implemented

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask
+# from crypt import methods
+# from urllib import request
+from flask import Flask,render_template,request,redirect
+from models import db,StudentModel
+
+
+
+
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return 'Hello World'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.db"
+app.config['SQLALCHMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+ 
+
+@app.route('/create', methods= ['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('create.html')
 
 app.run(host='localhost',  port=5000)
